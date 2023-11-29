6a40a4ca4076ebf068f52822855c941b8737a0a8
yeso1234 <137582457+yeso1234@users.noreply.github.com>
2023-11-29 14:17:11 +0900

    Update 계산기.c

--- 계산기.c
+++ 계산기.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+#include<math.h>
 float mul(float x,float y){
     return x*y;
 }
 float div(float x,float y){
     return x/y;
+}
+float fmodf(float x,float y){
+	return fmod(x,y);
 }
 main(){
     float a,b;
@@ -12,7 +16,7 @@ main(){
     printf("두 수를 입력하세요 : ");
     scanf("%f %f",&a,&b);
     
-    printf("4. 곱하기 5. 나누기 ");
+    printf("4. 곱하기 5. 나누기 6. 나머지 구하기 ");
     scanf("%d",&i);
     
     if(i==4){
@@ -20,5 +24,8 @@ main(){
     }
     else if(i==5){
         printf("%f",div(a,b));
+    }
+    else if(i==6){
+        printf("%f",fmodf(a,b));
     }
 }
